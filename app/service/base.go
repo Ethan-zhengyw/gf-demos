f162483de910c81fe5138640a61d912c69694c9f
郑仪旺 <zhengyiwang@lizhi.fm>
2021-04-08 16:13:41 +0800

    add order, page params; split SimpleCurdService.

--- app/service/base.go
+++ app/service/base.go
@@ -7,27 +7,61 @@ import (
 	"github.com/gogf/gf/net/ghttp"
 )
 
+type ListResult struct {
+	Data  gdb.Result `json:"data"`
+	Total int        `json:"total"`
+	Limit int        `json:"limit"`
+	Page  int        `json:"page"`
+	Count int        `json:"count"`
+}
 
 type tableNameGetter interface {
 	getTableName() string
+}
+
+type SimpleService struct {}
+
+type SimpleAddService struct {
+	SimpleService
+}
+
+type SimpleListService struct {
+	SimpleService
+	apiParamsParser web.ApiParameterParser
+}
+
+type SimpleDetailService struct {
+	SimpleService
+}
+
+type SimpleEditService struct {
+	SimpleService
+}
+
+type SimpleDeleteService struct {
+	SimpleService
 }
 
 // SimpleCurdService is for avoiding redundant code,
 // and curd method provided by SimpleCurdService can not return accurate (list of)model,
 // If result iof model type list needed, please use dao directly
 type SimpleCurdService struct {
-	apiParamsParser web.ApiParameterParser
+	SimpleAddService
+	SimpleListService
+	SimpleDetailService
+	SimpleEditService
+	SimpleDeleteService
 }
 
-func (s * SimpleCurdService) getTableName() string {
+func (s *SimpleService) getTableName() string {
 	return "unknown table"
 }
 
-func (s *SimpleCurdService) getGdbModel(tableName string) *gdb.Model {
-	return g.DB("default").Model(tableName).Safe()
+func (s *SimpleService) getGdbModel(tableName string) *gdb.Model {
+	return g.DB("default").Model(tableName).Safe(false)
 }
 
-func (s *SimpleCurdService) Add(tng tableNameGetter, r ...interface{}) error {
+func (s *SimpleAddService) Add(tng tableNameGetter, r ...interface{}) error {
 	gdbModel := s.getGdbModel(tng.getTableName())
 	if _, err := gdbModel.Insert(r); err != nil {
 		return err
@@ -35,25 +69,49 @@ func (s *SimpleCurdService) Add(tng tableNameGetter, r ...interface{}) error {
 	return nil
 }
 
-func (s *SimpleCurdService) List(tng tableNameGetter, r *ghttp.Request) (gdb.Result, error) {
-	filterParams := s.apiParamsParser.GetFilterParams(r)
-	searchConditions := s.apiParamsParser.GetSearchConditions(r)
+func (s *SimpleListService) List(tng tableNameGetter, r *ghttp.Request) (ListResult, error) {
 	gdbModel := s.getGdbModel(tng.getTableName())
-	return gdbModel.WherePri(searchConditions).FindAll(filterParams)
+
+	listParams := s.apiParamsParser.GetListParams(r)
+	lr := ListResult{Page: listParams.Page, Limit: listParams.Limit}
+
+	// query Total
+	if total, err := gdbModel.WherePri(listParams.FilterParams).WherePri(listParams.SearchConditions).Order(listParams.OrderName, listParams.Order).Count(); err != nil {
+		return lr, err
+	} else {
+		lr.Total = total
+	}
+
+	// query Count
+	if count, err := gdbModel.Page(listParams.Page, listParams.Limit).Count(); err != nil {
+		return lr, err
+	} else {
+		lr.Count = count
+	}
+
+	// query Data
+	if data, err := gdbModel.FindAll(); err != nil {
+		return lr, err
+	} else {
+		lr.Data = data
+	}
+
+	return lr, nil
 }
 
-func (s *SimpleCurdService) Edit(tng tableNameGetter, id int, r interface{}) error {
+func (s *SimpleEditService) Edit(tng tableNameGetter, id int, r interface{}) error {
 	gdbModel := s.getGdbModel(tng.getTableName())
 	if m, err := gdbModel.FindOne("id", id); err != nil || m == nil {
 		return err
-	}
-	if res, err := gdbModel.Update(r, "id", id); err != nil {
-		return err
+	} else {
+		if _, err := gdbModel.Update(r, "id", id); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func (s *SimpleCurdService) Delete(tng tableNameGetter, id int) error {
+func (s *SimpleDeleteService) Delete(tng tableNameGetter, id int) error {
 	gdbModel := s.getGdbModel(tng.getTableName())
 	if m, err := gdbModel.Delete("id", id); err != nil || m == nil {
 		return err
@@ -61,7 +119,7 @@ func (s *SimpleCurdService) Delete(tng tableNameGetter, id int) error {
 	return nil
 }
 
-func (s *SimpleCurdService) Detail(tng tableNameGetter, id int) (gdb.Record, error) {
+func (s *SimpleDetailService) Detail(tng tableNameGetter, id int) (gdb.Record, error) {
 	gdbModel := s.getGdbModel(tng.getTableName())
 	return gdbModel.FindOne("id", id)
 }
--- web/utils.go
+++ web/utils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"strconv"
 	"strings"
 )
 
@@ -13,14 +14,24 @@ const (
 )
 
 // ApiParameterParser help to parse request parameters
-type ApiParameterParser struct {}
+type ApiParameterParser struct { }
+
+// List parameter
+type ListParams struct {
+	FilterParams     g.Map
+	SearchConditions g.Map
+	OrderName        string
+	Order            string
+	Limit            int
+	Page             int
+}
 
 func (parser *ApiParameterParser) GetParamsWithPrefix(r *ghttp.Request, prefix string) g.Map {
 	paramsWithPrefix := make(g.Map)
 	for key, value := range r.GetQueryMap() {
 		if strings.HasPrefix(key, prefix) {
 			paramsWithPrefix[strings.TrimPrefix(key, prefix)] = value
-		t }
+		}
 	}
 	return paramsWithPrefix
 }
@@ -39,4 +50,63 @@ func (parser *ApiParameterParser) GetSearchConditions(r *ghttp.Request) g.Map {
 		searchConditions[fmt.Sprintf("`%v` like ?", key)] = fmt.Sprintf("%%%v%%", value)
 	}
 	return searchConditions
+}
+
+func (parser *ApiParameterParser) GetOrderParams(r *ghttp.Request) (orderName string, order string) {
+	const (
+		keyOrderName = "order_name"
+		keyOrder = "order"
+		defaultOrderName = "id"
+		defaultOrder = "desc"
+	)
+
+	// default order option
+	orderName, order = defaultOrderName, defaultOrder
+
+	for key, value := range r.GetQueryMap() {
+		if key == keyOrderName {
+			orderName = fmt.Sprintf("%v", value)
+		}
+		if key == keyOrder {
+			order = fmt.Sprintf("%v", value)
+		}
+	}
+
+	return orderName, order
+}
+
+func (parser *ApiParameterParser) GetPageParams(r *ghttp.Request) (limit int, page int) {
+	const (
+		keyLimit = "limit"
+		keyPage = "page"
+		defaultLimit = 10
+		defaultPage = 1
+	)
+
+	limit, page = defaultLimit, defaultPage
+
+	for key, value := range r.GetQueryMap() {
+		if key == keyLimit {
+			limit, _ = strconv.Atoi(value.(string))
+		}
+		if key == keyPage {
+			page, _ = strconv.Atoi(value.(string))
+		}
+	}
+
+	return limit, page
+}
+
+func (parser *ApiParameterParser) GetListParams(r *ghttp.Request) *ListParams  {
+	filterParams := parser.GetFilterParams(r)
+	searchConditions := parser.GetSearchConditions(r)
+	orderName, order := parser.GetOrderParams(r)
+	limit, page := parser.GetPageParams(r)
+
+	return &ListParams{
+		FilterParams:     filterParams,
+		SearchConditions: searchConditions,
+		OrderName:        orderName, Order:order,
+		Limit:limit, Page:page,
+	}
 }
